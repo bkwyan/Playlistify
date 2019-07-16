6a97edbdee5027e5caf87f194784ea02be5ea18d
Brandon Yan <byan54@uwo.ca>
2019-07-16 14:38:54 -0400

    Added utils

---
+++ src/components/utils/index.js
@@ -0,0 +1,10 @@
+//  Obtains parameters from the hash of the URL
+export const getHashParams() => {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+};
